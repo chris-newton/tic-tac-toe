4d3a514bd552d8dfac489ec8087207d5eec7002b
chris-newton <newton.chris685@gmail.com>
2024-09-25 16:10:46 -0400

    built win/end condition screen with modal, built reset button functionality, refactored game object back into a module

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const gameBoard = (function createGameBoard() {
     const isFull = () => emptySpaces === 0;
     const getBoard = () => board;
     const checkSpaceEmpty = (i, j) => !board[i][j];
+
     const getSpace = function(i, j) {
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
             throw {name : "Out of Bounds", message : "too lazy to implement"}; 
@@ -26,7 +27,12 @@ const gameBoard = (function createGameBoard() {
         emptySpaces--;
     }
 
-    return { isFull, getBoard, checkSpaceEmpty, getSpace, setSpace };
+    const clear = function() {
+        board = [["", "", ""],["", "", ""],["", "", ""]];
+        emptySpaces = 9;
+    };
+
+    return { isFull, getBoard, checkSpaceEmpty, getSpace, setSpace, clear };
 })();
 
 function createPlayer(name, mark) {
@@ -37,14 +43,9 @@ function createPlayer(name, mark) {
     return { getName, getMark };
 }
 
-let player1;
-let player2;
-let game;
 const modalForm = document.querySelector(".name-entry");
-
 modalForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
     [name1, name2] = Object.entries(formProps);
@@ -53,81 +54,107 @@ modalForm.addEventListener("submit", (e) => {
     player2 = createPlayer(name2[1], "O"); 
 
     const modal = document.querySelector("dialog");
-    modal.style.display = "none";
+    modal.replaceChildren(); // wipe the modal clean
+    modal.classList.toggle("hidden", true);
 
+    game.start(player1, player2);
+});
 
-    game = (function createGame(player1, player2) {
+const game = (function createGame() {
+    let outcome = null; // will be set to player1, player2, or "draw" by end of game
+    let turn = null;
+    let player1;
+    let player2;
+
+    const start = function(p1, p2) {
+        turn = p1;
+        player1 = p1;
+        player2 = p2;
+    }
+
+    const getTurn = () => turn;
+    const toggleTurn = () => turn = (turn === player1) ? player2 : player1;
+    const getOutcome = () => outcome;
+
+    // returns true iff player own
+    const checkPlayerWon = function(player) {
+        const mark = player.getMark();
         
-        let outcome = null; // will be set to player1, player2, or "draw" by end of game
-        let turn = player1;
-        const getTurn = () => turn;
-        const toggleTurn = () => turn = (turn === player1) ? player2 : player1;
-        const getOutcome = () => outcome;
-
-        // returns true iff player own
-        const checkPlayerWon = function(player) {
-            const mark = player.getMark();
-            
-            // check rows
-            for (let i = 0; i < 3; i++) {
-                if (gameBoard.getSpace(i, 0) === mark && 
-                    gameBoard.getSpace(i, 1) === mark && 
-                    gameBoard.getSpace(i, 2) === mark) {
-                    outcome = player;
-                    return true;
-                }
-            }
-            // check columns
-            for (let j = 0; j < 3; j++) {
-                if (gameBoard.getSpace(0, j) === mark && 
-                    gameBoard.getSpace(1, j) === mark && 
-                    gameBoard.getSpace(2, j) === mark) {
-                    outcome = player;
-                    return true;
-                }
-            }
-            // check diagonals
-            if (gameBoard.getSpace(0, 0) == mark &&
-                gameBoard.getSpace(1, 1) == mark && 
-                gameBoard.getSpace(2, 2) == mark) {
+        // check rows
+        for (let i = 0; i < 3; i++) {
+            if (gameBoard.getSpace(i, 0) === mark && 
+                gameBoard.getSpace(i, 1) === mark && 
+                gameBoard.getSpace(i, 2) === mark) {
                 outcome = player;
-                return true
-            } else if (gameBoard.getSpace(0, 2) == mark &&
-                    gameBoard.getSpace(1, 1) == mark &&
-                    gameBoard.getSpace(2, 0) == mark) {
-                outcome = player;
-                return true
-            }
-            return false;
-        }
-
-        // check game status
-        // returns true iff game has been won or draw
-        const isOver = function() {
-            // check draw
-            if (checkPlayerWon(player1) || checkPlayerWon(player2)) {
                 return true;
-            } 
-            return gameBoard.isFull();
-        }
-
-        const dispatchOutcome = function() {
-
-            switch (outcome) {
-                case player1:
-                    console.log(player1.getName() + " wins!");
-                    break;
-                case player2:
-                    console.log(player2.getName() + " wins!");
-                    break;
-                case null: 
-                    console.log("draw..");
             }
         }
+        // check columns
+        for (let j = 0; j < 3; j++) {
+            if (gameBoard.getSpace(0, j) === mark && 
+                gameBoard.getSpace(1, j) === mark && 
+                gameBoard.getSpace(2, j) === mark) {
+                outcome = player;
+                return true;
+            }
+        }
+        // check diagonals
+        if (gameBoard.getSpace(0, 0) == mark &&
+            gameBoard.getSpace(1, 1) == mark && 
+            gameBoard.getSpace(2, 2) == mark) {
+            outcome = player;
+            return true
+        } else if (gameBoard.getSpace(0, 2) == mark &&
+                gameBoard.getSpace(1, 1) == mark &&
+                gameBoard.getSpace(2, 0) == mark) {
+            outcome = player;
+            return true
+        }
+        return false;
+    }
+
+    // check game status
+    // returns true iff game has been won or draw
+    const isOver = function() {
+        // check draw
+        if (checkPlayerWon(player1) || checkPlayerWon(player2)) {
+            return true;
+        } 
+        return gameBoard.isFull();
+    }
+
+    const dispatchOutcome = function() {
+        const modal = document.querySelector("dialog");
+        const outcomeText = document.createElement("p");
+        outcomeText.classList.add("outcome-text");
         
-        return { getTurn, toggleTurn, getOutcome, isOver, dispatchOutcome };
-    })(player1, player2);
+        switch (outcome) {
+            case player1:
+            case player2:
+                outcomeText.innerText = outcome.getName() + " wins!";
+                break;
+            case null: 
+                outcomeText.innerText = "draw.";
+        }
+        modal.appendChild(outcomeText);
+        modal.classList.toggle("hidden");
+        modal.classList.add("modal-show-outcome");
+    }
+    
+    const modal = document.querySelector("dialog");
+    const resetButton = document.querySelector(".reset");
+    resetButton.addEventListener("click", () => {
+        outcome = null;
+        gameBoard.clear();
+        displayController.render();
+        start(player1, player2);
+        modal.replaceChildren();
+        modal.classList.toggle("hidden");
     });
+    
+    return { start, getTurn, toggleTurn, getOutcome, isOver, dispatchOutcome };
+})();
+
 
 // game logic
 const displayController = (function createDisplayController() {
@@ -144,7 +171,7 @@ const displayController = (function createDisplayController() {
         }
     }
 
-    // add click listeners to spaces    
+    // clicking a given space triggers the logic for a turn
     const buttons = document.querySelectorAll("button[class^='space'");
     buttons.forEach((button) => 
         button.addEventListener("click", (e) => {
